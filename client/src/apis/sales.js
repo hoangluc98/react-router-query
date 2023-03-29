11b0d5f5277331d5a2c04bd45361df8a5b836e6b
Lucas <luc.hoang@savvycomsoftware.com>
2023-03-29 10:23:31 +0700

    feat(api): change delay logic

--- client/src/apis/sales.js
+++ client/src/apis/sales.js
@@ -4,16 +4,19 @@ const API_URL = 'http://localhost:3000';
 const delay = (ms) => new Promise((res) => setTimeout(res, ms));
 
 export async function getSales() {
-  await delay(500);
-  return axios.get(`${API_URL}/sales`).then((res) => res.data);
+  const res = await axios.get(`${API_URL}/sales`);
+  await delay(3000);
+  return res.data;
 }
 
 export async function getSalesData(tab, config = {}) {
-  await delay(500);
-  return axios.get(`${API_URL}/sales-data?saleType=${tab}`, config).then((res) => res.data);
+  const res = await axios.get(`${API_URL}/sales-data?saleType=${tab}`, config);
+  await delay(3000);
+  return res.data;
 }
 
 export async function getSalesDetail(id, config = {}) {
-  await delay(500);
-  return axios.get(`${API_URL}/sales-data/${id}`, config).then((res) => res.data);
+  const res = await axios.get(`${API_URL}/sales-data/${id}`, config);
+  await delay(3000);
+  return res.data;
 }
